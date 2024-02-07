f43a4d050d194c318fe3c8f22bdc8a1014b7a123
Colby Neves <colby.neves@smail.astate.edu>
2024-02-07 11:35:57 -0600

    index change

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,19 @@ const { PORT, COOKIE_SECRET } = process.env;
 
 const SQLiteStore = connectSqlite3(session);
 
+app.use(
+    session({
+        store: new SQLiteStore({ db: 'sessions.sqlite' }),
+        secret: COOKIE_SECRET,
+        cookie: { maxAge: 8 * 60 * 60 * 1000 }, // 8 hours
+        name: 'session',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
+
+app.use(express.json());
+
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
 });
