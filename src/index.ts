d190c39fdb893689cd996f11c918b49bc3c64fd6
Colby Neves <colby.neves@smail.astate.edu>
2024-02-07 11:27:18 -0600

    index changes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
-import './config'; // Load environment variables
-import 'express-async-errors'; // Enable default error handling for async errors
-//test done
-//hi
+import './config';
+import 'express-async-errors';
+import express, { Express } from 'express';
+import session from 'express-session';
+import connectSqlite3 from 'connect-sqlite3';
+
+const app: Express = express();
+const { PORT, COOKIE_SECRET } = process.env;
+
+const SQLiteStore = connectSqlite3(session);
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+});
