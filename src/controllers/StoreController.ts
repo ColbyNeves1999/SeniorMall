a9307c455e2d39a6d5c794e73334aa2173ab4c5e
ColbyNeves1999 <colby.neves@smail.astate.edu>
2024-02-09 07:13:41 -0600

    Store Model/Controller created

---
+++ src/controllers/StoreController.ts
@@ -0,0 +1,7 @@
+//UNCOMMENT WHEN FUNCTIONS NEED TO BE MADE
+//import { Request, Response } from 'express';
+
+import {  } from '../models/StoreModel';
+
+
+export { };
---
+++ src/models/StoreModel.ts
@@ -0,0 +1,49 @@
+import { AppDataSource } from '../dataSource';
+import { Store } from '../entities/Store';
+
+const storeRepository = AppDataSource.getRepository(Store);
+
+async function addStore(mallname: string): Promise<Store> {
+
+    // Create the new user object and saves data
+    let newStore = new Store();
+    newStore.mallname = mallname;
+
+    // Then save it to the database
+    // NOTES: We reassign to `newUser` so we can access
+    // NOTES: the fields the database autogenerates (the id & default columns)
+    newStore = await storeRepository.save(newStore);
+
+    return newStore;
+
+}
+
+//Store's Location
+async function setMallLocation(storeId: string, location: string): Promise<void> {
+
+    let store = await getStoreById(storeId);
+    store.location = location;
+
+    store = await storeRepository.save(store);
+
+}
+
+//User's Admin Status
+async function setStorePhone(storeId: string, phone: string): Promise<void> {
+
+    let store = await getStoreById(storeId);
+    store.phone = phone;
+
+    store = await storeRepository.save(store);
+
+}
+
+async function getStoreById(storeId: string): Promise<Store | null> {
+    return await storeRepository.findOne({ where: { storeId } });
+}
+
+async function getStoreByLocation(location: string): Promise<Store | null> {
+    return storeRepository.findOne({ where: { location } });
+}
+
+export { addStore, setMallLocation, setStorePhone, getStoreById, getStoreByLocation };
