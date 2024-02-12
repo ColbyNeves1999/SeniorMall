0ce3365a08674c76a1c179b611ff2f14a59cb664
Cody Painter <cody.painter1@smail.astate.edu>
2024-02-12 11:38:18 -0600

    creating file for database schema

---
+++ database/schema.sql
@@ -0,0 +1,29 @@
+-- Purpose: contains schema for the project database
+
+CREATE TABLE IF NOT EXISTS User (
+    userId TEXT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    passwordHash TEXT UNIQUE NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    birthday INT NOT NULL,
+    admin BOOLEAN NOT NULL DEFAULT FALSE,
+);
+
+CREATE TABLE IF NOT EXISTS Mall (
+    storeId TEXT PRIMARY KEY,
+    mallName VARCHAR(255) UNIQUE NOT NULL DEFAULT 'Unknown Mall Name',
+    location VARCHAR(255) UNIQUE NOT NULL DEFAULT 'Unknown Mall Location',
+);
+
+CREATE TABLE IF NOT EXISTS Store (
+    storeId TEXT PRIMARY KEY,
+    storeName VARCHAR(255) UNIQUE NOT NULL DEFAULT 'Unknown Store Name',
+    location VARCHAR(255) UNIQUE NOT NULL DEFAULT 'Unknown Location',
+    phone VARCHAR(255) UNIQUE NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS Item (
+    itemId PRIMARY KEY DEFAULT,
+    itemName VARCHAR(255) NOT NULL DEFAULT 'Unknown Item Name',
+    stock INT NOT NULL
+);
