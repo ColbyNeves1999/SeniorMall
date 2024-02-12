d27e343417417c90c91ceae6f291f3f952a4b65b
hwiyoon_kim <hwiyoon.kim@smail.astate.edu>
2024-02-11 20:43:38 -0600

    connect to home

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -4,57 +4,49 @@ import { User } from '../entities/User';
 const userRepository = AppDataSource.getRepository(User);
 
 async function addUser(email: string, passwordHash: string): Promise<User> {
+  // Create the new user object and saves data
+  let newUser = new User();
+  newUser.email = email;
+  newUser.passwordHash = passwordHash;
 
-    // Create the new user object and saves data
-    let newUser = new User();
-    newUser.email = email;
-    newUser.passwordHash = passwordHash;
+  // Then save it to the database
+  // NOTES: We reassign to `newUser` so we can access
+  // NOTES: the fields the database autogenerates (the id & default columns)
+  newUser = await userRepository.save(newUser);
 
-    // Then save it to the database
-    // NOTES: We reassign to `newUser` so we can access
-    // NOTES: the fields the database autogenerates (the id & default columns)
-    newUser = await userRepository.save(newUser);
-
-    return newUser;
-
+  return newUser;
 }
 
-//User's associated Mall address
+// User's associated Mall address
 async function setUserMallAddress(userId: string, mallAddress: string): Promise<void> {
+  let user = await getUserById(userId);
+  user.address = mallAddress;
 
-    let user = await getUserById(userId);
-    user.address = mallAddress;
-
-    user = await userRepository.save(user);
-
+  user = await userRepository.save(user);
 }
 
-//User's Birthday
+// User's Birthday
 async function setUserBirthday(userId: string, birthday: number): Promise<void> {
+  let user = await getUserById(userId);
+  user.birthday = birthday;
 
-    let user = await getUserById(userId);
-    user.birthday = birthday;
-
-    user = await userRepository.save(user);
-
+  user = await userRepository.save(user);
 }
 
-//User's Admin Status
+// User's Admin Status
 async function setUserAdmin(userId: string, admin: boolean): Promise<void> {
+  let user = await getUserById(userId);
+  user.admin = admin;
 
-    let user = await getUserById(userId);
-    user.admin = admin;
-
-    user = await userRepository.save(user);
-
+  user = await userRepository.save(user);
 }
 
 async function getUserById(userId: string): Promise<User | null> {
-    return await userRepository.findOne({ where: { userId } });
+  return await userRepository.findOne({ where: { userId } });
 }
 
 async function getUserByEmail(email: string): Promise<User | null> {
-    return userRepository.findOne({ where: { email } });
+  return userRepository.findOne({ where: { email } });
 }
 
 export { addUser, setUserMallAddress, setUserBirthday, setUserAdmin, getUserByEmail };
