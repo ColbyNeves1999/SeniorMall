087e0c5825779f25d637f2057abb24b234619bdb
Colby Neves <colby.neves@smail.astate.edu>
2024-03-27 11:14:28 -0500

    Uncommenting some code for admin updates

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ import {
   deleteAccount,
   renderProfilePage,
   updateUserPassword,
-  // updateUserAdminStatus,
+  updateUserAdminPermissions,
 } from './controllers/UserController';
 
 import { storeCreator, renderStoreAnalysisPage } from './controllers/StoreController';
@@ -69,7 +69,7 @@ app.get('/userHomepage', userHomePage); // Displays the user's homepage
 app.post('/createStore', storeCreator);
 app.post('/createitem', itemCreator);
 app.post('/itemStockModifier', itemStockModifier);
-// app.post('/elevate', updateUserAdminStatus);
+app.post('/elevate', updateUserAdminPermissions);
 
 app.get('/storeInfo', renderStoreAnalysisPage);
 
