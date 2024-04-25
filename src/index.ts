5356b1fab9331d0a930d7e00d804e83ad4c828cb
ColbyNeves1999 <120044465+ColbyNeves1999@users.noreply.github.com>
2024-04-24 22:18:51 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ function iMakeSureAdminsExist() {
   lookForAdmin();
 }
 
-scheduleJob('15 03 * * *', iMakeSureAdminsExist);
+scheduleJob('19 03 * * *', iMakeSureAdminsExist);
 
 app.use(sessionMiddleware);
 app.use(express.json());
