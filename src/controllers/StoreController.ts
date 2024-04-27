922bc411ed738039938c999553389fad760db225
ColbyNeves1999 <120044465+ColbyNeves1999@users.noreply.github.com>
2024-04-27 13:29:50 -0500

    Update StoreController.ts

--- src/controllers/StoreController.ts
+++ src/controllers/StoreController.ts
@@ -76,7 +76,7 @@ async function renderHeldPage(req: Request, res: Response): Promise<void> {
 async function renderStoreAnalysisPage(req: Request, res: Response): Promise<void> {
   //try {
     const stores = await getAllStores(); // Retrieve all stores from the database
-    res.render('storeInfo', { stores });
+  //  res.render('storeInfo', { stores });
   //} catch (error) {
   //  console.error(error);
   //  res.status(500).send('Internal server error');
