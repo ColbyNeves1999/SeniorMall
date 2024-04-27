00f6a15986d172486a2de93b7aa429d92c5b0133
ColbyNeves1999 <120044465+ColbyNeves1999@users.noreply.github.com>
2024-04-27 15:45:17 +0000

    Update StoreController.ts

--- src/controllers/StoreController.ts
+++ src/controllers/StoreController.ts
@@ -74,13 +74,13 @@ async function renderHeldPage(req: Request, res: Response): Promise<void> {
 }
 
 async function renderStoreAnalysisPage(req: Request, res: Response): Promise<void> {
-  try {
+  //try {
     const stores = await getAllStores(); // Retrieve all stores from the database
     res.render('storeInfo', { stores });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal server error');
-  }
+  //} catch (error) {
+  //  console.error(error);
+  //  res.status(500).send('Internal server error');
+  //}
 }
 
 // Define the ChartData interface
