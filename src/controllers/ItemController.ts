f1fa6d647400f049315a9536d142bf150db1e75d
ColbyNeves1999 <120044465+ColbyNeves1999@users.noreply.github.com>
2024-04-29 13:06:30 -0500

    Coments

--- src/controllers/ItemController.ts
+++ src/controllers/ItemController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import { getItemByName, addItem, updateItemStock, deleteItemById } from '../models/ItemModel';
 
+//Takes information from request body and creates a new item
 async function itemCreator(req: Request, res: Response): Promise<void> {
   const { itemName, itemStock, itemDescription, storeName } = req.body as NewItemRequest;
 
@@ -17,7 +18,9 @@ async function itemCreator(req: Request, res: Response): Promise<void> {
   res.redirect('/users/userAccountsPage');
 }
 
+//Modifies item stocks and calls for removal if the stock is 0 or below
 async function itemStockModifier(req: Request, res: Response): Promise<void> {
+  
   // Store name is taken for verification purposes that will be added later
   const { itemName, itemStock, itemDescription, storeName } = req.body as NewItemRequest;
   const { subOrAdd } = req.body as { subOrAdd: string };
