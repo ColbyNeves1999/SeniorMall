9c524086691d67dba25c4534c604b15131c9b72e
ColbyNeves1999 <120044465+ColbyNeves1999@users.noreply.github.com>
2024-04-29 13:37:06 -0500

    Comments

--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,7 @@ function iMakeSureAdminsExist() {
   lookForAdmin();
 }
 
+//Daily makes sure that an admin exists AND the GMAIL_USER is an admin
 scheduleJob('25 03 * * *', iMakeSureAdminsExist);
 
 app.use(sessionMiddleware);
@@ -61,43 +62,43 @@ app.use(express.static('public', { extensions: ['html'] }));
 app.set('view engine', 'ejs');
 // endpoints
 
-app.get('/', renderMainPage); //Generates an ejs for the main page
-
-app.post('/storePage', renderStorePage);
-app.post('/heldPage', renderHeldPage);
-app.get('/cart', renderCart);
-
 app.get('/api/users', getAllUserProfiles);
 
-
+//Creation/destruction of sessions
 app.post('/login', logIn); // Lets a user login
 app.get('/logout', logOut); // Lets a user logout
 
-app.get('/users/userAccountsPage', renderProfilePage);
-
-app.post('/users/delete', deleteAccount);
-app.post('/users/changePassword', updateUserPassword);
-app.post('/users/changeEmail', updateUserEmail);
-app.get('/cartPage', renderCart); //Renders a user's card
-
-app.get('/userHomepage', userHomePage); // Displays the user's homepage
-
+// Changes that are being made to user accounts
 app.post('/registerUser', registerUser); // Registers a user
+app.post('/elevate', updateUserAdminPermissions); // Handles user admin (de)elevation
+app.post('/users/delete', deleteAccount); // Deletion of accounts
+app.post('/users/changePassword', updateUserPassword); // Updating of passwords
+app.post('/users/changeEmail', updateUserEmail); // Updating of email
+
+// Functions handling rendering done
+app.get('/', renderMainPage); //Generates an ejs for the main page
+app.post('/storePage', renderStorePage); //Store .ejs display
+app.post('/heldPage', renderHeldPage); // Items being held .ejs display
+app.get('/cart', renderCart); // User's cart page .ejs
+app.get('/users/userAccountsPage', renderProfilePage); //Displays user's .ejs page
+app.get('/userHomepage', userHomePage); // Displays the user's static homepage
+
+// Handles store creation and item management
 app.post('/createStore', storeCreator); // Creates stores
 app.post('/createitem', itemCreator); // Creates items
+app.post('/itemStockModifier', itemStockModifier); // Handles modifying stock
 
+// Store rendering page for anything that is displayed on store analysis
+app.get('/storeInfo', renderStoreAnalysisPage); //Renders the page with store data .ejs
+app.get('/store/chart', generateStoreChart); // Handles the rendering of the store chart
+app.get('/cartPage', renderCart); //Renders a user's cart
 
-app.post('/itemStockModifier', itemStockModifier);
-app.post('/elevate', updateUserAdminPermissions);
+//Functions handling carts
+app.post("/addToCart", addItemToCart); //Adds individual items from cart
+app.post('/removeFromCart', removeItemFromCart); //Removes individual items from cart
+app.post('/closeOrder', closingOrder); //Closes cart requests
 
-app.get('/storeInfo', renderStoreAnalysisPage);
-app.get('/store/chart', generateStoreChart);
-
-app.post("/addToCart", addItemToCart)
-app.post('/removeFromCart', removeItemFromCart);
-
-app.post('/closeOrder', closingOrder);
-
+//Test for checking stores in database
 app.get('/getthemstores', getAllStoreProfiles);
 
 app.listen(PORT, () => {
