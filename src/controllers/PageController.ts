028d15007a44fe2c8dd4e910f084ddde4ff7f10e
ColbyNeves1999 <120044465+ColbyNeves1999@users.noreply.github.com>
2024-04-29 13:07:08 -0500

    Comments

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 
+//exists to allow for transition to .ejs version of main page
 async function renderMainPage(req: Request, res: Response): Promise<void> {
   const { isLoggedIn } = req.session;
 
